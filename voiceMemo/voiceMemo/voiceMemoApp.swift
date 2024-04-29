53ac53e557d7e3f785537421a428ee77e797d67c
hyejin <6oohye@naver.com>
2024-04-29 14:40:49 +0900

    [update] create Swift file

---
+++ voiceMemo/voiceMemo/voiceMemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  voiceMemoApp.swift
+//  voiceMemo
+//
+//  Created by 6혜진 on 4/29/24.
+//
+
+import SwiftUI
+
+@main
+struct voiceMemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
