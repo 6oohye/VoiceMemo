2a6dc61d58d8881283295430cdc6fe292566210f
hyejin <6oohye@naver.com>
2024-05-03 18:34:29 +0900

    [update] 음성메모 viewModel 완성

--- voiceMemo/voiceMemo/Feature/Home/VoiceRecoder/VoiceRecoderView.swift
+++ voiceMemo/voiceMemo/Feature/Home/VoiceRecoder/VoiceRecoderView.swift
@@ -23,35 +23,35 @@ struct VoiceRecoderView: View {
 
 //MARK: - 타이틀 뷰
 private struct TitleView : View {
-   fileprivate var body: some View {
-       HStack{
-           Text("음성메모")
-               .font(.system(size: 30, weight: .bold))
-               .foregroundColor(.customBlack)
-           Spacer()
-       }
-       .padding(.horizontal, 30)
-       .padding(.top, 30)
+    fileprivate var body: some View {
+        HStack{
+            Text("음성메모")
+                .font(.system(size: 30, weight: .bold))
+                .foregroundColor(.customBlack)
+            Spacer()
+        }
+        .padding(.horizontal, 30)
+        .padding(.top, 30)
     }
 }
 
 //MARK: - 음성메모 안내뷰
 private struct AnnouncementView:View {
-   fileprivate var body: some View {
-       VStack(spacing: 15){
-           Rectangle()
-               .fill(Color.customCoolGray)
-               .frame(height: 1)
-           Spacer()
-               .frame(height: 180)
-           
-           Image("pencil")
-               .renderingMode(.template)
-           Text("현재 등록된 음성메모가 없습니다.")
-           Text("하단의 녹음버튼을 눌러 음성메모를 시작해주세요.")
-       }
-       .font(.system(size: 16))
-       .foregroundColor(.customGray2)
+    fileprivate var body: some View {
+        VStack(spacing: 15){
+            Rectangle()
+                .fill(Color.customCoolGray)
+                .frame(height: 1)
+            Spacer()
+                .frame(height: 180)
+            
+            Image("pencil")
+                .renderingMode(.template)
+            Text("현재 등록된 음성메모가 없습니다.")
+            Text("하단의 녹음버튼을 눌러 음성메모를 시작해주세요.")
+        }
+        .font(.system(size: 16))
+        .foregroundColor(.customGray2)
     }
 }
 
@@ -71,10 +71,26 @@ private struct VoiceRecoderListView: View {
                     .frame(height: 1)
                 
                 ForEach(voiceRecoderViewModel.recordedFiles, id: \.self){recordedFile in
-                        //음성메모 셀 뷰 호출
+                    //음성메모 셀 뷰 호출
                 }
             }
         }
+    }
+}
+
+//MARK: - 음성메모 셀 뷰 구현
+private struct VoiceRecoderCellView: View {
+    @ObservedObject private var voiceRecoderViewModel: VoiceRecoderViewModel
+    
+    private var recordrdFile : URL
+    private var creationDate : Date?
+    private var duration : TimeInterval?
+    private var progressBarValue: Float{
+        if voiceRecoderViewModel.selectedRecordedFile == 
+    }
+    
+    fileprivate var body: some View {
+        /*@START_MENU_TOKEN@*//*@PLACEHOLDER=Hello, world!@*/Text("Hello, world!")/*@END_MENU_TOKEN@*/
     }
 }
 
